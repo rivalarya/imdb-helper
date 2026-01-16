ff2d1f9c1a3a1f42f0bbc1367a5613f99c328533
Rival Arya <rifall.aryap@gmail.com>
2026-01-17 02:59:17 +0700

    feat: migrate genres

---
+++ migrations/import_genres.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const client = require('./db');
+const copyFrom = require('pg-copy-streams').from;
+
+async function run() {
+    await client.connect();
+
+    const query = `
+        COPY genres (name)
+        FROM STDIN WITH (FORMAT text)
+    `;
+
+    const fileStream = fs.createReadStream('unique_genres.txt');
+    const pgStream = client.query(copyFrom(query));
+
+    fileStream.pipe(pgStream);
+
+    pgStream.on('finish', () => {
+        console.log('COPY finished');
+        client.end();
+    });
+
+    pgStream.on('error', err => {
+        console.error('COPY error:', err);
+        client.end();
+    });
+}
+
+run().catch(console.error);
